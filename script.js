00bba60ab0333a0926713d37a084d63125435392
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-05-30 23:01:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
-    //Write your code here
+
+	let table = document.getElementById("sampleTable");
+
+	// insert a new row at the top (index 0)
+	let row = table.insertRow(0);
+
+	// insert two cells
+	let cell1 = row.insertCell(0);
+	let cell2 = row.insertCell(1);
   
-  
+	cell1.innerHTML = "New Cell1";
+	cell2.innerHTML = "New Cell2";
 }
